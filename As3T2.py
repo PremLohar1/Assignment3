4fe5d5f14477fe5dfa33244ef6c75b1bd1085ebb
Prem lohar <loharprem046@gmail.com>
2025-09-30 21:11:47 +0530

    Add files via upload

---
+++ As3T2.py
@@ -0,0 +1,15 @@
+import math
+num=int(input('Enter The Number '))
+print('\n')
+if num>=0:
+    root=math.sqrt(num)
+    print('The Squre Root Is = ',root)
+else:
+    print('Enter The Positive Number')    
+    
+lognum = math.log(num) 
+print('Natural logarithm = ',lognum) 
+
+sinnum = math.sin(num) 
+print('Sine of the number = ',sinnum) 
+    
